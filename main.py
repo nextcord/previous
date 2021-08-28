dd1f4f624827c52d502f6ee31a0413323b47523c
rf20008 <64931063+rf20008@users.noreply.github.com>
2021-08-28 18:53:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ async def on_message(message):
     if (result := issue_regex.search(message.content)):
         issue_id = result.groups()[0]
         await message.channel.send(f"https://github.com/nextcord/nextcord/issues/{issue_id}")
+       
 
-@bot.command()
+@bot.command(help="TODO")
 async def todo(ctx):
     await ctx.send("https://github.com/nextcord/nextcord/projects/1 and going through all the issues")
+    
+@bot.command(help="Returns the ping of this bot", brief = "Returns the ping of this bot")
+async def ping(ctx):
+    await ctx.send(f"Pong! My ping is {round(bot.latency*1000000)/1000000} microseconds.")
+    
+@bot.command(help="Adds a todo item", brief = "Adds todo")
+
+    
 
 bot.run(env["TOKEN"])
