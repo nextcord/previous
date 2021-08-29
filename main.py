0a493271d395e3413ad8809e9cb38d3a3a1bc664
Tag-Epic <tagepicuwu@gmail.com>
2021-08-29 12:23:14 +0200

    Feat: Add a regex for discord issues

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from os import environ as env
 from re import compile
 
 bot = commands.Bot("=")
+bot.load_extension("jishaku")
 
 issue_regex = compile(r"##(\d+)")
+discord_regex = compile(r"##(\d+)")
 
 @bot.listen()
 async def on_message(message):
     if (result := issue_regex.search(message.content)):
         issue_id = result.groups()[0]
         await message.channel.send(f"https://github.com/nextcord/nextcord/issues/{issue_id}")
+    if (result := discord_regex.search(message.content)):
+        issue_id = result.groups()[0]
+        await message.channel.send(f"https://github.com/rapptz/discord.py/issues/{issue_id}")
 
 @bot.command()
 async def todo(ctx):
