826942fefda661003324f51498f360e9f51ff987
Tag-Epic <tagepicuwu@gmail.com>
2021-08-30 22:19:26 +0200

    Feat: Help cog

---
+++ cogs/help.py
@@ -0,0 +1,48 @@
+from nextcord import ui, ButtonStyle, ChannelType, Interaction, MessageType
+from nextcord.ext import commands
+
+class HelpView(ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+    # Nextcord help
+    @ui.button(label="Nextcord help", style=ButtonStyle.red, custom_id="help:nextcord")
+    async def nextcord_help(self, _, interaction: Interaction):
+        await self.create_help_thread("Nextcord", interaction)
+    @ui.button(label="D.py libs help", style=ButtonStyle.gray, custom_id="help:dpylibs")
+    async def nextcord_lib_help(self, _, interaction: Interaction):
+        await self.create_help_thread("Discord.py libraries", interaction)
+    
+    # Python
+    @ui.button(label="Python help", style=ButtonStyle.green, custom_id="help:python")
+    async def python_help(self, _, interaction: Interaction):
+        await self.create_help_thread("Python", interaction)
+    async def create_help_thread(self, name, interaction):
+        await interaction.channel.create_thread(name=f"{name} help ({interaction.user})", type=ChannelType.public_thread)
+        thread = await interaction.response.send_message("Created!", ephemeral=True)
+        if thread is not None: # Bug in nextcord atm
+            await thread.add_user(interaction.user)
+
+class HelpCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.help_channel = 881965127031722004
+        self.bot.loop.create_task(self.create_view())
+
+    async def create_view(self):
+        if getattr(self.bot, "help_view_set", False) is False:
+            self.bot.help_view_set = True
+            self.bot.add_view(HelpView())
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == self.help_channel and message.type == MessageType.thread_created:
+            await message.delete(delay=5)
+
+
+    @commands.command()
+    @commands.is_owner()
+    async def help_menu(self, ctx):
+        await ctx.send("Click a button to create a help thread!", view=HelpView())
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
