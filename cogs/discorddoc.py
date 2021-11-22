fe131b973666138f8231bd6fe43c2054060d0dea
Soheab <soheabdebeste@hotmail.com>
2021-11-22 18:06:10 +0100

    Revert "Revert "Merge branch 'close-thread-on-leave-fix' of https://github.com/Soheab/previous into close-thread-on-leave-fix""
    This reverts commit 131977aff51415ed3da24295e9c493fcb81cdece.

---
+++ cogs/discorddoc.py
@@ -0,0 +1,39 @@
+import nextcord
+from nextcord.ext import commands
+from algoliasearch.search_client import SearchClient
+
+class DiscordHelp(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        ## Fill out from trying a search on the ddevs portal
+        app_id = "BH4D9OD16A"
+        api_key = "f37d91bd900bbb124c8210cca9efcc01"
+        self.search_client = SearchClient.create(app_id, api_key)
+        self.index = self.search_client.init_index('discord')
+    @commands.command()
+    async def ddoc(self, ctx, *, search_term):
+        results = await self.index.search_async(search_term)
+        description = ""
+        hits = []
+        for hit in results["hits"]:
+            title = self.get_level_str(hit["hierarchy"])
+            if title in hits:
+                continue
+            hits.append(title)
+            url = hit["url"].replace("https://discord.com/developers/docs", "https://discord.dev")
+            description += f"[{title}]({url})\n"
+            if len(hits) == 10:
+                break
+        embed = nextcord.Embed(title="Your help has arrived!", description=description, color=nextcord.Color.random())
+        await ctx.send(embed=embed)
+
+    def get_level_str(self, levels):
+        last = ""
+        for level in levels.values():
+            if level is not None:
+                last = level
+        return last
+
+
+def setup(bot):
+    bot.add_cog(DiscordHelp(bot))
