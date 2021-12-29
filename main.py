d66b9cb8c97502bf871d46a767db4b3e830f8a2c
Leestarb Original <56854192+leestarb@users.noreply.github.com>
2021-12-29 18:54:53 +0600

    Fix indentation

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
     elif os.path.isfile(filename):
-            print(f"Unable to load {filename[:-3]}")
+        print(f"Unable to load {filename[:-3]}")
 
 
 async def startup():
