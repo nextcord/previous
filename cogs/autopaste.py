edecd92c9b23c390c589a69af84b884a62574fa0
Soheab <soheabdebeste@hotmail.com>
2022-01-06 21:23:15 +0100

    better code yes

--- cogs/autopaste.py
+++ cogs/autopaste.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List
 import re
 
 from nextcord import Message
@@ -10,22 +10,25 @@ codeblock_regex = re.compile(r"`{3}(\w*) *\n(.*)\n`{3}", flags=re.DOTALL)
 discord_to_workbin = {
     "py": "python",
     "js": "javascript",
-    "text/plain": "text",
-    "text/markdown": "markdown",
-    "text/x-python": "python",
-    "application/json": "json",
-    "application/javascript": "javascript",
 }
+
 other_paste_services = [
     "pastebin.com",
 ]
-supported_content_types: Tuple[str, ...] = (
+supported_content_types: List[str] = [
     "text/plain",
     "text/markdown",
     "text/x-python",
     "application/json",
     "application/javascript",
-)
+]
+content_type_to_lang = {
+    "text/plain": "text",
+    "text/markdown": "markdown",
+    "text/x-python": "python",
+    "application/json": "json",
+    "application/javascript": "javascript"
+}
 
 class AutoPaste(Cog):
     def __init__(self, bot) -> None:
@@ -34,8 +37,6 @@ class AutoPaste(Cog):
     async def do_upload(self, content: str, language: str) -> str:
         res = await self.bot.session.post("https://paste.nextcord.dev/api/new", data=str(content))  # type: ignore
         paste_id = (await res.json())["key"]
-
-        language = discord_to_workbin.get(language, language)
         return f"https://paste.nextcord.dev/?id={paste_id}&language={language}"
 
     @Cog.listener()
@@ -47,20 +48,26 @@ class AutoPaste(Cog):
         if message.content.startswith("!"):
             return
 
-        if message.attachments and message.attachments[0].content_type:
+        if message.attachments:
             first_attachment = message.attachments[0]
-            assert first_attachment.content_type is not None
-            if first_attachment.content_type.startswith(supported_content_types):
-                read_text_file = await first_attachment.read()
-                if not bool(read_text_file.decode('utf-8')):
-                    # file is empty
-                    return
+            if not first_attachment.content_type:
+                return
+            
+            content_type = first_attachment.content_type.split(";")[0].strip()
+            if content_type not in supported_content_types:
+                return
 
-                file_content = str(read_text_file.decode('utf-8'))
-                language = first_attachment.content_type.replace("; charset=utf-8", "")
-                url = await self.do_upload(file_content, language)
-                await message.reply(f"Please avoid files for code. Posted to {url}", allowed_mentions=AllowedMentions.none())
-                return 
+            file_bytes = await first_attachment.read()
+            if not bool(file_bytes.decode('utf-8')):
+                # file is empty
+                return
+
+            file_content = str(file_bytes.decode('utf-8'))
+            language = content_type_to_lang.get(content_type, "text")
+            
+            url = await self.do_upload(file_content, language)
+            await message.reply(f"Please avoid files for code. Posted to {url}", allowed_mentions=AllowedMentions.none())
+            return 
 
         regex_result = codeblock_regex.search(message.content)
         if regex_result is None:
@@ -70,6 +77,7 @@ class AutoPaste(Cog):
             return
 
         language = regex_result.group(1) or "python"
+        language = discord_to_workbin.get(language, language)
         code = regex_result.group(2)
 
         url = await self.do_upload(code, language)
