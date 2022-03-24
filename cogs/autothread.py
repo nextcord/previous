b6590a6f3dd9c4970b7e56869b79bfee8e3209d6
koala9712 <86164003+koala9712@users.noreply.github.com>
2022-03-24 17:55:36 +0530

    Resolve

--- cogs/autothread.py
+++ cogs/autothread.py
@@ -1,20 +1,20 @@
 from nextcord.ext import commands
 from nextcord import Message
 
-AUTO_THREAD_CHANNEL: int = 937840096021979236
+AUTO_THREAD_CHANNEL = 937840096021979236
 
 
 class AutoThread(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        self.bot: commands.bot = bot
+        self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message: Message):
+    async def on_message(self, message):
         if message.author.bot: # return if the author of the message is a bot.
             return
         if message.channel.id == AUTO_THREAD_CHANNEL: # check if the message channel is the one we want to auto threads.
             await message.create_thread(name="Discussion")
 
 
-def setup(bot: commands.bot):
+def setup(bot: commands.Bot):
     bot.add_cog(AutoThread(bot))
