62478b15942b61d46c3bd4536029d51549e12fde
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-05-30 09:46:08 +0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -30,7 +30,7 @@ HELP_CHANNEL_ID: int = int(env["HELP_CHANNEL_ID"])
 HELP_LOGS_CHANNEL_ID: int = int(env["HELP_LOG_CHANNEL_ID"])
 HELPER_ROLE_ID: int = int(env["HELP_NOTIFICATION_ROLE_ID"])
 HELP_MOD_ID: int = int(env["HELP_MOD_ROLE_ID"])
-NO_HELP_ID: int = int(env["NO_HELP_ROLE_ID"])
+HELP_BAN_ID: int = int(env["HELP_BANNED_ROLE_ID"])
 GUILD_ID: int = int(env["GUILD_ID"])
 CUSTOM_ID_PREFIX: str = "help:"
 NAME_TOPIC_REGEX: str = r"^(?P<topic>.*?) \((?P<author>[^)]*[^(]*)\)$"
@@ -232,7 +232,7 @@ class HelpView(ui.View):
         self.add_item(HelpButton("Python", style=ButtonStyle.green, custom_id="python"))
     
     async def interaction_check(self, interaction: Interaction):
-        if interaction.user.get_role(NO_HELP_ID) is not None:
+        if interaction.user.get_role(HELP_BANNED_ID) is not None:
             await interaction.send(NO_HELP_MESSAGE, ephemeral=True)
             return False
 
