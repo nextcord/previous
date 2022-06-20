792cb56439a24c7eb6841bc87428bb1dd85ec993
ooliver1 <oliverwilkes2006@icloud.com>
2022-06-20 21:26:12 +0100

    refactor(ddocs): fix up style and shorten function

--- previous/cogs/discorddoc.py
+++ previous/cogs/discorddoc.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
 import nextcord
 from algoliasearch.search_client import SearchClient
 from nextcord.ext import commands
-from typing import TYPE_CHECKING
+from nextcord.ext.commands import Context
 
 if TYPE_CHECKING:
     from previous.__main__ import Previous
@@ -16,22 +20,26 @@ class DiscordHelp(commands.Cog):
         self.search_client = SearchClient.create(app_id, api_key)
         self.index = self.search_client.init_index("discord")
 
-    @commands.command(help="discord api docs")
-    async def ddoc(self, ctx, *, search_term):
-        results = await self.index.search_async(search_term)
+    @commands.command(help="Search the Discord API Docs")
+    async def ddoc(self, ctx: Context, *, search_term: str):
+        results: dict[str, Any] = await self.index.search_async(search_term)  # type: ignore
         description = ""
-        hits = []
+        hits: list[str] = []
         for hit in results["hits"]:
             title = self.get_level_str(hit["hierarchy"])
+
             if title in hits:
                 continue
+
             hits.append(title)
             url = hit["url"].replace(
                 "https://discord.com/developers/docs", "https://discord.dev"
             )
             description += f"[{title}]({url})\n"
+
             if len(hits) == 10:
                 break
+
         embed = nextcord.Embed(
             title="Your help has arrived!",
             description=description,
@@ -39,12 +47,8 @@ class DiscordHelp(commands.Cog):
         )
         await ctx.send(embed=embed)
 
-    def get_level_str(self, levels):
-        last = ""
-        for level in levels.values():
-            if level is not None:
-                last = level
-        return last
+    def get_level_str(self, levels: dict[str, str]) -> str:
+        return next((x for x in levels.values() if x is not None), "")
 
 
 def setup(bot: Previous):
