a8e8f5b6184b58425af0c1967c77f6f04776e0e8
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-06-28 09:26:54 -0700

    feat: tag command

---
+++ cogs/tags.py
@@ -0,0 +1,22 @@
+import aiohttp
+
+from nextcord.ext import commands
+
+class Tags(commands.Cog):
+    @commands.command()
+    async def tag(self, ctx, *, name: str):
+        """Grabs the Markdown file for a tag."""
+        branch_name = "master" # TODO: Dynamically grab current branch name via git
+        markdown_link = f"https://raw.githubusercontent.com/nextcord/previous/{branch_name}/assets/{name}.md"
+
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(markdown_link) as resp:
+                # Exists
+                if resp.status == 200:
+                    return await ctx.send(markdown_link)
+                # Not Found
+                elif resp.status == 404:
+                    return await ctx.send("Tag with that name does not exist.")
+                # Some other error
+                else:
+                    return await ctx.send(f"Unable to grab a tag with that name. Got HTTP code {resp.status}.")
